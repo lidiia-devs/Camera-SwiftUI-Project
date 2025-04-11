5a83e891c4c9370af0e8528756b2a3f2daa37670
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-04-10 21:32:22 -0400

    Initial Commit

---
+++ Camera SwiftUI Project/Camera_SwiftUI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_SwiftUI_ProjectApp.swift
+//  Camera SwiftUI Project
+//
+//  Created by Lidiia Diachkovskaia on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Camera_SwiftUI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
