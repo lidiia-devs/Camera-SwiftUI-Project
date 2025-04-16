39c5c0f522f21e70a8b7d2a34404f4cc89860d2c
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-04-15 22:09:08 -0400

    ImageView was added

---
+++ Camera SwiftUI Project/ImagesView/ImageItem.swift
@@ -0,0 +1,8 @@
+//
+//  ImageItem.swift
+//  Camera SwiftUI Project
+//
+//  Created by Lidiia Diachkovskaia on 4/15/25.
+//
+
+import Foundation
---
+++ Camera SwiftUI Project/ImagesView/ImagesView.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesView.swift
+//  Camera SwiftUI Project
+//
+//  Created by Lidiia Diachkovskaia on 4/15/25.
+//
+
+import Foundation
